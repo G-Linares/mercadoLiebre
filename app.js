cd821b5beeaeb1bcf03c7b4d0e764a6d7a1171b1
G-Linares <gerrylinares@gmail.com>
2021-11-12 19:40:16 -0600

    make it better

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.listen(3000, () => console.log('Servidor Arriba en puerto 3000'));
+
+app.use(express.static('public'));
+
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/home.html'));
+});
+
+app.get('/register',function(req,res){
+    res.sendFile(path.resolve(__dirname,'views/register.html'));
+});
+
+app.get('/login',function (req,res){
+    res.sendFile(path.resolve(__dirname,'views/login.html'));    
+});
