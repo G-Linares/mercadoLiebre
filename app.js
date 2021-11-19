95f5090dff422d163999c405fa7d6c09fed972e4
G-Linares <gerrylinares@gmail.com>
2021-11-18 20:27:04 -0600

    First commit

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3000, () => console.log('Servidor Arriba en puerto 3000'));
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`Servidor en puerto 3000`);
+})
 
 app.use(express.static('public'));
 
